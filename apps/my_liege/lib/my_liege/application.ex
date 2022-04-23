2ee992d9b9245782a69c3f22190e9e2bee07a727
Alessandro Di Maria <adm@m42.ch>
2022-04-23 13:05:29 +0200

    start and stop sim

--- apps/my_liege/lib/my_liege/application.ex
+++ apps/my_liege/lib/my_liege/application.ex
@@ -14,9 +14,9 @@ defmodule MyLiege.Application do
         Sim.Realm.Supervisor,
         name: MyLiege.Game,
         domain_services: [
-          {MyLiege.Service.Admin, partition: :admin, max_demand: 1}
+          {MyLiege.Service.Admin, partition: :admin, max_demand: 1},
+          {MyLiege.Service.Sim, partition: :sim, max_demand: 1}
           # {MyLiege.Service.User, partition: :user, max_demand: 1},
-          # {MyLiege.Service.Sim, partition: :sim, max_demand: 1}
         ],
         reducers: [MyLiege.PubSubReducer]
       }
--- apps/my_liege_web/lib/my_liege_web/live/board_live/index.ex
+++ apps/my_liege_web/lib/my_liege_web/live/board_live/index.ex
@@ -5,13 +5,56 @@ defmodule MyLiegeWeb.BoardLive.Index do
 
   def mount(_params, _session, socket) do
     if connected?(socket), do: subscribe()
-    {:ok, socket}
+    {:ok, assign(socket, started: MyLiege.started?())}
   end
 
   def render(assigns) do
     ~H"""
     <h1>My Liege - Board</h1>
+    <.start_button started={@started}></.start_button>
     """
+  end
+
+  def start_button(assigns) do
+    label = if assigns.started, do: "stop", else: "start"
+
+    ~H"""
+    <button phx-click="run-sim" phx-value-running={"#{@started}"}><%= label %></button>
+    """
+  end
+
+  def handle_event("run-sim", %{"running" => "true"}, socket) do
+    Logger.info("event stop sim")
+    MyLiege.stop_sim()
+    {:noreply, socket}
+  end
+
+  def handle_event("run-sim", %{"running" => "false"}, socket) do
+    Logger.info("event start sim")
+    MyLiege.start_sim()
+    {:noreply, socket}
+  end
+
+  def handle_info({:sim_started, started: started}, socket) do
+    Logger.info("sim started #{started}")
+    {:noreply, assign(socket, started: started)}
+  end
+
+  def handle_info({:workplaces_produced, :time_unit}, socket) do
+    Logger.info("workplaces_produced")
+    {:noreply, socket}
+  end
+
+  def handle_info({:inventory_updated, input: _input}, socket) do
+    Logger.info("inventory_updated")
+    {:noreply, socket}
+  end
+
+  def handle_info({:error, message}, socket) do
+    {:noreply,
+     socket
+     |> clear_flash(:info)
+     |> put_flash(:error, message)}
   end
 
   defp subscribe() do
