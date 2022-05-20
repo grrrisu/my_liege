f0152c7f4be7f54e882dcdb56582dbcc8efcac8c
Alessandro Di Maria <adm@m42.ch>
2022-05-20 19:38:57 +0200

    consume material while producing

--- apps/my_liege/lib/my_liege/game/workplace.ex
+++ apps/my_liege/lib/my_liege/game/workplace.ex
@@ -5,32 +5,38 @@ defmodule MyLiege.Game.Workplace do
 
   def produce(%Workplace{pawn: nil}), do: {nil, nil}
 
-  def produce(
-        %Workplace{
-          type: :construction_site,
-          inventory: %{manpower: x},
-          input: %{manpower: x}
-        } = construction_site
-      ) do
-    {%Workplace{construction_site.output | id: construction_site.id}, nil}
+  def produce(%Workplace{} = workplace) do
+    with true <- has_material?(workplace) do
+      next_step(workplace)
+    else
+      _ -> {nil, nil}
+    end
   end
 
-  def produce(
-        %Workplace{type: _factory, inventory: %{manpower: x} = inventory, input: %{manpower: x}} =
-          farm
-      ) do
-    farm = %Workplace{farm | inventory: %{inventory | manpower: 0}}
-    {farm, farm.output}
+  defp next_step(
+         %Workplace{
+           type: :construction_site,
+           inventory: %{manpower: x},
+           input: %{manpower: x}
+         } = construction_site
+       ) do
+    {construct_site(construction_site), nil}
   end
 
-  def produce(%Workplace{type: _factory, inventory: %{manpower: x} = inventory} = farm) do
-    farm = %Workplace{farm | inventory: %{inventory | manpower: x + 1}}
-    {farm, nil}
+  defp next_step(
+         %Workplace{type: _factory, inventory: %{manpower: x}, input: %{manpower: x}} = factory
+       ) do
+    produce_output(factory)
   end
 
-  def produce(%Workplace{type: _factory, inventory: inventory} = farm) do
-    farm = %Workplace{farm | inventory: Map.put(inventory, :manpower, 1)}
-    {farm, nil}
+  defp next_step(%Workplace{type: _factory, inventory: %{manpower: x} = inventory} = factory) do
+    factory = %Workplace{factory | inventory: %{inventory | manpower: x + 1}}
+    {factory, nil}
+  end
+
+  defp next_step(%Workplace{type: _factory, inventory: inventory} = factory) do
+    factory = %Workplace{factory | inventory: Map.put(inventory, :manpower, 1)}
+    {factory, nil}
   end
 
   def has_capacity?(%Workplace{pawn: nil}), do: true
@@ -38,8 +44,41 @@ defmodule MyLiege.Game.Workplace do
 
   def get_pawns(%Workplace{pawn: nil}), do: 0
   def get_pawns(%Workplace{pawn: _}), do: 1
-end
 
-defmodule MyLiege.Game.Blueprint do
-  defstruct type: nil, needed_resources: %{}, manpower: 0
+  def has_material?(%Workplace{inventory: inventory, input: input}) do
+    input
+    |> without_manpower()
+    |> Enum.all?(fn {key, value} ->
+      Map.get(inventory, key, 0) >= value
+    end)
+  end
+
+  defp consume_matrial(%Workplace{inventory: inventory, input: input}) do
+    input
+    |> without_manpower()
+    |> Enum.reduce(inventory, fn {key, value}, inventory ->
+      Map.update!(inventory, key, fn stored -> stored - value end)
+    end)
+  end
+
+  defp without_manpower(map) do
+    Enum.reject(map, fn {key, _} -> key == :manpower end)
+  end
+
+  defp construct_site(construction_site) do
+    inventory = consume_matrial(construction_site)
+
+    %Workplace{
+      construction_site.output
+      | id: construction_site.id,
+        pawn: construction_site.pawn,
+        inventory: %{inventory | manpower: 0}
+    }
+  end
+
+  defp produce_output(factory) do
+    inventory = consume_matrial(factory)
+    factory = %Workplace{factory | inventory: %{inventory | manpower: 0}}
+    {factory, factory.output}
+  end
 end
--- apps/my_liege/test/my_liege/services/sim/workplace_production_test.exs
+++ apps/my_liege/test/my_liege/services/sim/workplace_production_test.exs
@@ -15,23 +15,47 @@ defmodule MyLiege.Service.Sim.WorkplaceProductionTest do
       assert Enum.count(events) == 0
     end
 
+    test "not enough material in inventory" do
+      {%{1 => workplace}, events} =
+        WorkplaceProduction.workplaces_production(%{
+          1 => %Workplace{id: 1, type: :farm, inventory: %{tools: 1}, input: %{tools: 2}}
+        })
+
+      assert Map.get(workplace.inventory, :manpower) |> is_nil()
+      assert Enum.count(events) == 0
+    end
+
     test "starting work" do
       {%{1 => workplace}, events} =
         WorkplaceProduction.workplaces_production(%{
-          1 => %Workplace{id: 1, type: :farm, pawn: %Pawn{}}
+          1 => %Workplace{
+            id: 1,
+            type: :farm,
+            pawn: %Pawn{},
+            inventory: %{tools: 2},
+            input: %{tools: 2}
+          }
         })
 
       assert workplace.inventory.manpower == 1
+      assert workplace.inventory.tools == 2
       assert [{:workplaces_updated, ids: [1]}] == events
     end
 
     test "ongoing work" do
       {%{1 => workplace}, events} =
         WorkplaceProduction.workplaces_production(%{
-          1 => %Workplace{id: 1, type: :farm, inventory: %{manpower: 2}, pawn: %Pawn{}}
+          1 => %Workplace{
+            id: 1,
+            type: :farm,
+            inventory: %{manpower: 2, tools: 2},
+            input: %{tools: 2},
+            pawn: %Pawn{}
+          }
         })
 
       assert workplace.inventory.manpower == 3
+      assert workplace.inventory.tools == 2
       assert [{:workplaces_updated, ids: [1]}] == events
     end
 
@@ -41,17 +65,39 @@ defmodule MyLiege.Service.Sim.WorkplaceProductionTest do
           1 => %Workplace{
             id: 1,
             type: :farm,
-            inventory: %{manpower: 3},
-            input: %{manpower: 3},
+            inventory: %{manpower: 3, tools: 2},
+            input: %{manpower: 3, tools: 2},
             output: %{food: 5},
             pawn: %Pawn{}
           }
         })
 
       assert workplace.inventory.manpower == 0
+      assert workplace.inventory.tools == 0
 
       assert [{:command, {:sim, :add_inventory, %{food: 5}}}, {:workplaces_updated, [ids: [1]]}] ==
                events
+    end
+  end
+
+  describe "construction site" do
+    test "finnished work" do
+      {%{1 => workplace}, events} =
+        WorkplaceProduction.workplaces_production(%{
+          1 => %Workplace{
+            id: 1,
+            type: :construction_site,
+            inventory: %{manpower: 3, wood: 5},
+            input: %{manpower: 3, wood: 5},
+            pawn: %Pawn{},
+            output: %Workplace{type: :farm}
+          }
+        })
+
+      assert %Workplace{id: 1, type: :farm, pawn: %Pawn{}, inventory: %{wood: 0, manpower: 0}} =
+               workplace
+
+      assert [{:workplaces_updated, [ids: [1]]}] == events
     end
   end
 end
